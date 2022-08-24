999d19398c97fbbb2d888f59c26a5487bb52017d
gmanojisaac <gmanoj.isaac@gmail.com>
2022-08-24 10:56:12 +0530

    Saved correctly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MarkdownService } from 'ngx-markdown';
-import { HttpClient } from '@angular/common/http';
-import { MermaidAPI } from 'ngx-markdown';
+
 
 @Component({
   selector: 'app-root',
@@ -26,25 +24,14 @@ export class AppComponent implements OnInit {
   
   ### Blockquote
   > Blockquote to the max`;
-  markdown!: string | undefined;
 
-  constructor(private mdService: MarkdownService, private http: HttpClient) { 
+
+  constructor() { 
    
   }
 
   ngOnInit() {
-    this.http.get('../assets/myfile.md', 
-    { responseType: 'text' }).subscribe((getdata: any) =>{
-      this.markdown = this.mdService.parse(getdata);
-    });
-  }
 
-
-  public options: MermaidAPI.Config = {
-    fontFamily: '"trebuchet ms", verdana, arial, sans-serif',
-    logLevel: MermaidAPI.LogLevel.Info,
-    theme: MermaidAPI.Theme.Dark
-  };
 }
-
+}
 
