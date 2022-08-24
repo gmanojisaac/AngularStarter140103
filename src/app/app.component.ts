27bee2c7b13d52b31a3fa0d0b3bb71701d869053
gmanojisaac <gmanoj.isaac@gmail.com>
2022-08-24 10:45:49 +0530

    Mermaid Working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { MarkdownService } from 'ngx-markdown';
+import { HttpClient } from '@angular/common/http';
+import { MermaidAPI } from 'ngx-markdown';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularStarter140103';
-  markdown = `## Markdown __rulez__!
+  markdowncode = `## Markdown __rulez__!
   ---
   
   ### Syntax highlight
@@ -23,7 +26,25 @@ export class AppComponent {
   
   ### Blockquote
   > Blockquote to the max`;
+  markdown!: string | undefined;
 
+  constructor(private mdService: MarkdownService, private http: HttpClient) { 
+   
+  }
+
+  ngOnInit() {
+    this.http.get('../assets/myfile.md', 
+    { responseType: 'text' }).subscribe((getdata: any) =>{
+      this.markdown = this.mdService.parse(getdata);
+    });
+  }
+
+
+  public options: MermaidAPI.Config = {
+    fontFamily: '"trebuchet ms", verdana, arial, sans-serif',
+    logLevel: MermaidAPI.LogLevel.Info,
+    theme: MermaidAPI.Theme.Dark
+  };
 }
 
 
